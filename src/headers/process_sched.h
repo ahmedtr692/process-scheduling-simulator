b31e0d35a0187c4cd4f9a4d40f0dde595f92ee98
ahmedtr692 <ahmedtr692@gmail.com>
2025-10-12 17:38:11 +0100

    implement the process struct

---
+++ src/headers/process_sched.h
@@ -0,0 +1,15 @@
+#ifndef PROCESS
+#define PROCESS
+
+
+typedef struct{
+  char * process_name ;
+  int date;
+  int duration;
+  int priority;
+ }process;
+void fifo_proc(process* p){
+
+}
+
+#endif // !DEBUGdef 
