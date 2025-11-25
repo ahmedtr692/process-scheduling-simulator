1779fc213a9c31ac7d0e9ff828786c0490a67941
ahmedtr692 <ahmedtr692@gmail.com>
2025-11-25 19:11:03 +0100

    .

--- src/headers/basic_sched.c
+++ src/headers/basic_sched.c
@@ -1,18 +1,93 @@
 #include "basic_sched.h"
 
-process_queue fifo_sched(process_queue p) {
-    process_queue sorted = p;
-    
-    for (node_t *i = sorted.head; i != NULL; i = i->next) {
-        for (node_t *j = i->next; j != NULL; j = j->next) {
-            if (i->proc.arrival_time_p > j->proc.arrival_time_p) {
-                process_t temp = i->proc;
-                i->proc = j->proc;
-                j->proc = temp;
+void fifo_sched(process_queue* p, process_descriptor_t* descriptor[], int mode) {
+    int calc_time = 0;
+    int io_time = 0;
+    int index = 0;
+    node_t *current = p->head;
+
+    if (mode == 0) {
+        while (current != NULL) {
+            process_t proc = current->proc;
+
+            for (int i = 0; i < proc.descriptor_p.count; i++) {
+                if (proc.descriptor_p.operations[i].operation_p == calc_p) {
+                    descriptor[index]->date = calc_time;
+                    descriptor[index]->state = running_p;
+                    descriptor[index]->operation = calc_p;
+                    calc_time += proc.descriptor_p.operations[i].duration_op;
+                }
+                else if (proc.descriptor_p.operations[i].operation_p == IO_p) {
+                    descriptor[index]->date = io_time;
+                    descriptor[index]->state = running_p;
+                    descriptor[index]->operation = IO_p;
+                    io_time += proc.descriptor_p.operations[i].duration_op;
+                }
+                index++;
             }
+
+            current = current->next;
         }
     }
-    
-    return sorted;
+    else if (mode == 1) {
+        if (p->head != NULL) {
+            node_t *first = p->head;
+            p->head = p->head->next;
+            first->next = NULL;
+            p->tail->next = first;
+            p->tail = first;
+        }
+    }
 }
 
+void round_robin(process_queue* p, int quantum, process_descriptor_t* descriptor[]) {
+    int calc_time = 0;
+    int io_time = 0;
+    int index = 0;
+
+    while (p->size > 0) {
+        node_t *current = p->head;
+        process_t proc = current->proc;
+
+        int op_idx = -1;
+        for (int i = 0; i < proc.descriptor_p.count; i++) {
+            if (proc.descriptor_p.operations[i].duration_op > 0) {
+                op_idx = i;
+                break;
+            }
+        }
+
+        if (op_idx == -1) {
+            fifo_sched(p, descriptor, 1);
+            continue;
+        }
+
+        int time_slice = (proc.descriptor_p.operations[op_idx].duration_op > quantum)
+                         ? quantum
+                         : proc.descriptor_p.operations[op_idx].duration_op;
+
+        if (proc.descriptor_p.operations[op_idx].operation_p == calc_p) {
+            descriptor[index]->date = calc_time;
+            descriptor[index]->state = running_p;
+            descriptor[index]->operation = calc_p;
+            calc_time += time_slice;
+        }
+        else if (proc.descriptor_p.operations[op_idx].operation_p == IO_p) {
+            descriptor[index]->date = io_time;
+            descriptor[index]->state = running_p;
+            descriptor[index]->operation = IO_p;
+            io_time += time_slice;
+        }
+
+        current->proc.descriptor_p.operations[op_idx].duration_op -= time_slice;
+        index++;
+
+        fifo_sched(p, descriptor, 1);
+    }
+}
+
+void shortest_job_first(process_queue* p, process_descriptor_t* descriptor[]) {
+}
+
+void priority_sched(process_queue* p, int preemptive, process_descriptor_t* descriptor[]) {
+}
--- src/headers/basic_sched.h
+++ src/headers/basic_sched.h
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// this defines the states that the process can be in
 enum process_state {
   new_p,
   waiting_p,
@@ -14,28 +13,22 @@ enum process_state {
   blocked_p
 };
 
-// this enum defines the operations that can be done by the process
 typedef enum {
-  calc_p,     // logical and arithmetic operations
-  IO_p,       // memory, read/write and input/output operations
-  none        // the process does nothing
+  calc_p,
+  IO_p,
+  none
 } process_operation_t;
 
-// this new type is defined to describe each sequence of the process
 typedef struct operation_t {
   process_operation_t operation_p;
   int duration_op;
-  // int critical; // this will be uncommented in case we implement semaphore or mutex
 } operation_t;
 
-// descriptor of the process like 2 units CPU, 3 units I/O and 8 units CPU
-// which is an array of operations
 typedef struct descriptor_op {
-  operation_t *operations;  // dynamically allocated array
+  operation_t *operations;
   int count;
 } descriptor_op;
 
-// process structure
 typedef struct process_t {
   char *process_name;
   int begining_date;
@@ -45,13 +38,11 @@ typedef struct process_t {
   int priority_p;
 } process_t;
 
-// linked list node structure
 typedef struct node_t {
   process_t proc;
   struct node_t *next;
 } node_t;
 
-// queue structure for process scheduling
 typedef struct process_queue {
   node_t *head;
   node_t *tail;
@@ -64,8 +55,7 @@ typedef struct process_descriptor_t {
   process_operation_t operation;
 }process_descriptor_t;
 
-// Function declarations
-void fifo_sched(process_queue* p, process_descriptor_t* descriptor[]);
+void fifo_sched(process_queue* p, process_descriptor_t* descriptor[], int mode);
 
 void round_robin(process_queue* p, int quantum, process_descriptor_t* descriptor[]);
 
@@ -73,4 +63,4 @@ void shortest_job_first(process_queue* p, process_descriptor_t* descriptor[]);
 
 void priority_sched(process_queue* p, int preemptive, process_descriptor_t* descriptor[]);
 
-#endif // BASIC_SCHED_H
+#endif
