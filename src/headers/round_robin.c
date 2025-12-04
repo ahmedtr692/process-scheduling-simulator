6ba1c933731a7ce28dc98a874592d8b30f3790c2
ahmedtr692 <ahmedtr692@gmail.com>
2025-12-04 11:30:23 +0100

    round robin

--- src/headers/round_robin.c
+++ src/headers/round_robin.c
@@ -1,3 +1,42 @@
 #include "basic_sched.h"
 
-round 
+void round_robin_sched(process_queue* p, process_descriptor_t** descriptor, int size, quantum)
+{
+  if (p->size == 0 ) return ;
+  
+    process_queue *rr_queue = malloc(sizeof(process_queue));
+    rr_queue->head = NULL;
+    rr_queue->tail = NULL;
+    rr_queue->size = 0;
+
+
+    node_t *current = p->head;
+    while (current != NULL) {
+        add_tail(rr_queue, current->proc);
+        current = current->next;
+    }
+
+
+    if (rr_queue->size > 1) {
+        int swapped;
+        node_t *cur;
+        node_t *last = NULL;
+
+        do {
+            swapped = 0;
+            cur = rr_queue->head;
+            while (cur->next != last) {
+                if (cur->proc.begining_date > cur->next->proc.begining_date) {
+                    process_t tmp = cur->proc;
+                    cur->proc = cur->next->proc;
+                    cur->next->proc = tmp;
+                    swapped = 1;
+                }
+                cur = cur->next;
+            }
+            last = cur;
+        } while (swapped);
+    }
+  
+  
+}
